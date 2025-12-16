32683654121fa777610e2967dcc266f36f40444d
libozhou <libozhou@kuaishou.com>
2025-12-16 21:11:12 +0800

    add cambrian-s

--- test/test_spatial_reasoning.py
+++ test/test_spatial_reasoning.py
@@ -7,8 +7,9 @@ sys.path.append(".")
 
 
 
-from src.sceneflow.reasoning.spatial_reasoning.spatial_vlm.spatial_ladder import SpatialLadder
-from src.sceneflow.reasoning.spatial_reasoning.spatial_vlm.spatial_reasoner import SpatialReasoner
+from src.sceneflow.reasoning.spatial_reasoning.spatial_ladder.spatial_ladder import SpatialLadder
+from src.sceneflow.reasoning.spatial_reasoning.spatial_reasoner.spatial_reasoner import SpatialReasoner
+from src.sceneflow.reasoning.spatial_reasoning.cambrian_s.cambrian_s import CambrianS
 
 model_path=""
 #spatialladder=SpatialLadder.from_pretrained("")
