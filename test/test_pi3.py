d84470e3082d390f371da385803dbf5f1d28f8d9
Jianbin Zhao <1518272584@mail.dlut.edu.cn>
2026-02-25 14:09:07 +0800

    fix: update test_pi3.py with both models, remove Chinese comments in prope.py

--- test/test_pi3.py
+++ test/test_pi3.py
@@ -3,11 +3,10 @@ sys.path.append("..")
 
 from sceneflow.pipelines.pi3.pipeline_pi3 import Pi3Pipeline
 
-# Pi3X (multimodal, recommended)
+# --- Test Pi3X (multimodal, recommended) ---
 DATA_PATH = "../data/test_case1/ref_image.png"
 MODEL_PATH = "yyfz233/Pi3X"
-OUTPUT_DIR = "output_pi3"
-INTERACTION = "point_cloud_generation"
+OUTPUT_DIR = "output_pi3x"
 
 pipeline = Pi3Pipeline.from_pretrained(
     representation_path=MODEL_PATH,
@@ -16,7 +15,23 @@ pipeline = Pi3Pipeline.from_pretrained(
 
 results = pipeline(
     DATA_PATH,
-    interaction=INTERACTION,
+    interaction="point_cloud_generation",
 )
 
 results.save(OUTPUT_DIR)
+
+# --- Test Pi3 ---
+MODEL_PATH_PI3 = "yyfz233/Pi3"
+OUTPUT_DIR_PI3 = "output_pi3"
+
+pipeline_pi3 = Pi3Pipeline.from_pretrained(
+    representation_path=MODEL_PATH_PI3,
+    model_type="pi3",
+)
+
+results_pi3 = pipeline_pi3(
+    DATA_PATH,
+    interaction="point_cloud_generation",
+)
+
+results_pi3.save(OUTPUT_DIR_PI3)
